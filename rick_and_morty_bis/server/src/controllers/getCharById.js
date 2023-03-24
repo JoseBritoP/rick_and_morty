be2b6a665267beba4f14d605992cf49a9dcfdd31
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-03-24 15:26:50 -0300

    Optimizando promesa

--- rick_and_morty_bis/server/src/controllers/getCharById.js
+++ rick_and_morty_bis/server/src/controllers/getCharById.js
@@ -2,20 +2,21 @@ const axios = require("axios")
 const URL_BASE = 'https://be-a-rym.up.railway.app/api'
 const KEY = '25646530d133.e16d02b2bb0d7f53a40c'
 
-const success = (response)=>{
+const successH = (response,res)=>{
   const {id,name,gender,status,species,image } = response.data;
   res.writeHead(200,{ "Content-Type": "application/json"});
   res.end(JSON.stringify({id,name,gender,status,species,image }))
 }
 
+const errorH = (error,res)=>{
+  res.writeHead(500,{"Content-Type":"text/plain"})
+  res.end(error.message)
+}
 const getCharById = (res,id) => {
 
   axios.get(`${URL_BASE}/character/${id}?key=${KEY}`)
   .then(response => success(response,res))
-  .catch((error)=>{
-    res.writeHead(500,{"Content-Type":"text/plain"})
-    res.end(error.message)
-  })
+  .catch(error => errorH(error,res))
 
 }
 
